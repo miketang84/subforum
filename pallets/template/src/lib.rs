0c743c21dc92ea957eba7c16353097c3c1adc581
Daogang Tang <daogangtang@gmail.com>
2022-03-10 19:34:44 +0800

    wip, add article_post call.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,79 +1,86 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode};
+use sp_io::offchain_index;
+use sp_runtime::{offchain::storage::StorageValueRef, RuntimeDebug};
+
+mod model;
+mod article;
+
+use model::{
+    Article,
+};
+
 // Re-export pallet items so that they can be accessed from the crate namespace.
 pub use pallet::*;
+
+const WRITE_KEY_PREFIX: &[u8] = b"WRITEP";
+
 
 #[frame_support::pallet]
 pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use sp_std::vec::Vec; // Step 3.1 will include this in `Cargo.toml`
+    use sp_std::vec::Vec;
 
-    /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }    
     
-    // Pallets use events to inform users when important changes are made.
-    // Event documentation should end with an array that provides descriptive names for parameters.
-    // https://docs.substrate.io/v3/runtime/events-and-errors
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        /// Event emitted when a proof has been claimed. [who, claim]
-        ClaimCreated(T::AccountId, Vec<u8>),
-        /// Event emitted when a claim is revoked by the owner. [who, claim]
-        ClaimRevoked(T::AccountId, Vec<u8>),
+        ArticleIndexInserted(T::AccountId, Vec<u8>),
+        ArticleIndexUpdated(T::AccountId, Vec<u8>),
     }
 
-    #[pallet::error]   // <-- Step 4. code block will replace this.
+    #[pallet::error]
     pub enum Error<T> {
-        /// The proof has already been claimed.
-        ProofAlreadyClaimed,
-        /// The proof does not exist, so it cannot be revoked.
-        NoSuchProof,
-        /// The proof is claimed by another account, so caller can't revoke it.
-        NotProofOwner,
+        ArticleIndexInsertError,
+        ArticleIndexUpdateError,
     }
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
-    #[pallet::storage] // <-- Step 5. code block will replace this.
-    pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
-
+    // a pair of article_id vec<u8> -> article_item hash vec<u8>
+    #[pallet::storage]
+    pub(super) type ArticleIdHashMap = 
+        StorageMap<_, Blake2_128Concat, Vec<u8>, Vec<u8>, ValueQuery>;  
+   
+    // the article post increasing counter, default 0
+    #[pallet::storage]
+    pub(super) type ArticleNonce = StorageValue<_, u64, ValueQuery>;
+    // #[pallet::type_value]
+    // pub(super) fn ArticleNonceDefault() -> u64 { 0 }
+    // pub(super) type ArticleNonce =
+    //    StorageValue<Value = u64, QueryKind = ValueQuery, OnEmpty = ArticleNonceDefault>;
+    
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-    // Dispatchable functions allow users to interact with the pallet and invoke state changes.
-    // These functions materialize as "extrinsics", which are often compared to transactions.
-    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+
         #[pallet::weight(1_000)]
-        pub fn create_claim(
+        pub fn article_post(
             origin: OriginFor<T>,
-            proof: Vec<u8>,
+            new_article_objbin: Vec<u8>,
             ) -> DispatchResult {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://docs.substrate.io/v3/runtime/origins
             let sender = ensure_signed(origin)?;
+            //let sender = ensure_root(origin)?;
 
-            // Verify that the specified proof has not already been claimed.
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+            // self increasing
+            ArticleNonce::mutate(|nonce| nonce + 1);
 
-            // Get the block number from the FRAME System pallet.
-            let current_block = <frame_system::Pallet<T>>::block_number();
-
-            // Store the proof with the sender and block number.
-            Proofs::<T>::insert(&proof, (&sender, current_block));
-
-            // Emit an event that the claim was created.
-            Self::deposit_event(Event::ClaimCreated(sender, proof));
+            // get latest nonce and write to local storage
+            let nonce = ArticleNonce::get();
+            let storage_key = WRITE_KEY_PREFIX.to_string() + ":" + "article_post:" + &nonce.to_string();
+            offchain_index::set(&storage_key, &new_article_objbin);
+            let storage_key = WRITE_KEY_PREFIX.to_string() + ":" + "article_post:top";
+            offchain_index::set(&storage_key, &nonce.to_string());
 
             Ok(())
         }
